38a845422001369888a332bbdfce718743772136
kunduruvenu <kunduruvenu@gmail.com>
2022-12-02 19:18:15 +0530

    f

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Flipkart from "./Components/Flipkart";
+import Weather from "./Components/Weather";
+
+function App() {
+  return (
+    <div>
+      <center>
+        <Weather />
+        <Flipkart />
+      </center>
+    </div>
+  );
+}
+
+export default App;
